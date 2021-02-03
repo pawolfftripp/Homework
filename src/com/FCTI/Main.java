79ff8e58b8230856f752309cbcb85b64ce2db721
haswell <perry.wolfftripp@gmail.com>
2021-02-03 14:47:31 -0500

    Correct file 2.1.2021

--- src/com/FCTI/Main.java
+++ src/com/FCTI/Main.java
@@ -9,30 +9,71 @@ import java.util.stream.IntStream;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner userInput = new Scanner(System.in);
 
         ArrayList<Integer> numberArray = new ArrayList<Integer>();
 
-        for (int i = 0; i < 5 ;i++) {
+
+        for (int i = 0; i < 5; i++) {
             System.out.print("Please enter a number: ");
             String addNumber = userInput.nextLine();
             int my_num = Integer.parseInt(addNumber);
             numberArray.add(my_num);
         }
-//Finds the sum, product, largest, and smallest of those numbers
+
+        System.out.println(numberArray);
 
 
-        System.out.println(numberArray.get(0));
-        System.out.println(numberArray.get(1));
+//SUM
+        int sum = 0;
 
+        for (int i : numberArray)
+            sum += i;
+        System.out.println("The sum is: " + sum);
+
+
+//PRODUCT
+        int product = 1;
+        for (int i = 0; i < numberArray.size(); i++) {
+            product = product * numberArray.get(i);
+//             System.out.println(product);
+        }
+        System.out.println("The product is: " + product);
+
+
+//LARGEST
+        int max = numberArray.get(0);
+        for (int i = 0; i < numberArray.size(); i++) {
+            if (numberArray.get(i) > max)
+                max = numberArray.get(i);
+        }
+        System.out.println("The largest number is: " + max);
+
+
+//SMALLEST
+        int min = numberArray.get(0);
+        for (int i = 0; i < numberArray.size(); i++) {
+            if (numberArray.get(i) < min)
+                min = numberArray.get(i);
+        }
+        System.out.println("The smallest number is: " + min);
+
+        System.out.println();
 
         HashMap<String, String> vehicleInv = new HashMap<String, String>();
 
-        vehicleInv.put("Mazda", "Miata");
-        vehicleInv.put("Volkswagen", "GTI");
-        vehicleInv.put("Audi", "S3" );
-        vehicleInv.put("Honda", "Civic");
-        vehicleInv.put("Subaru", "WRX");
+        vehicleInv.put("Miata", "Mazda");
+        vehicleInv.put("GTI", "Volkswagen");
+        vehicleInv.put("S3", "Audi");
+        vehicleInv.put("Civic", "Honda");
+        vehicleInv.put("WRX", "Subaru");
 
+        System.out.println("What vehicle model are you looking for? ");
+        String modelRequest = userInput.nextLine();
+        if (vehicleInv.containsKey(modelRequest)) ;
+        {
+            System.out.printf("It appears you are looking for a %s, our %s section is over here...", modelRequest, vehicleInv.get(modelRequest));
+        }
     }
 }
